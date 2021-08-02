f675882d604c6fde918c45ebcfc3bbb442fb90ac
Michael Martinez <zeroeclipse@live.com>
2021-08-02 15:17:38 -0700

    Redoing models and etc with scaffold generator.

--- db/migrate/20210802221638_create_items.rb
+++ db/migrate/20210802221638_create_items.rb
@@ -1,9 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-      t.string :name
-      t.float :price
-      t.integer :category_id
+
       t.timestamps
     end
   end
