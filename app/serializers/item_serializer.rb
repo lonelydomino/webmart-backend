92803446af39bf5e08a3e904d8d59f7178a23e7c
Michael Martinez <zeroeclipse@live.com>
2021-08-05 19:37:08 -0700

    Updating items serializer.

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,10 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  include Rails.application.routes.url_helpers
+  attributes :id, :name, :price, :image
   belongs_to :category, except: [:updated_at, :created_at, :items]
+  
+  def image
+    rails_blob_path(object.image, disposition: "attachment", only_path: true) if object.image.attached?
+    # object.image.service_url if object.image.attached?
+  end
 end
